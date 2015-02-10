6846c55cabb609b0aacdb3451ad8682ac7aeec54
kirmaniarslan <arslankirmani9600@gmail.com>
2015-02-10 20:16:16 +0100

    Boot message :fix compilation & bold italic text
    Change-Id: I2bbdeb7a10545d9047d7d35c6084364172cd062c

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -71,6 +71,7 @@ import android.service.dreams.DreamService;
 import android.service.dreams.IDreamManager;
 import android.speech.RecognizerIntent;
 import android.telecom.TelecomManager;
+import android.text.Html;
 import android.service.gesture.EdgeGestureManager;
 import com.android.internal.os.DeviceKeyHandler;
 
@@ -6348,7 +6349,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     // Only display the current package name if the main message says "Optimizing app N of M".
                     // We don't want to do this when the message says "Starting apps" or "Finishing boot", etc.
 //                    mBootMsgDialog.setMessage(msg + "\n" + currentPackageName);
-                    mBootMsgDialog.setMessage(Html.fromHtml(msg + "<br><b>" + currentPackageName + "</b>"));
+                    mBootMsgDialog.setMessage(Html.fromHtml(msg + "<br><i><b>" + currentPackageName + "</b></i>"));
                 }
                 else {
                     mBootMsgDialog.setMessage(msg);
